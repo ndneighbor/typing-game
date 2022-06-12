892f2113a246752016e2f23b1e1e841abe4393dc
ndneighbor <asara019@fiu.edu>
2022-06-11 23:22:31 -0400

    push it all up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import type from "./type.svg";
+import { useState } from "react";
+import "./App.css";
+import { generate } from "./utils/words";
+import { currentTime } from "./utils/time";
+import useKeyPress from "./hooks/useKeyPress";
+// In figma trainer, I can use a hook for parts of the game logic!
+// Generates words for the typing test
+const initialWords = generate();
 
 function App() {
+  const [leftPadding, setLeftPadding] = useState(
+    new Array(20).fill(" ").join("")
+  );
+  const [outgoingChars, setOutgoingChars] = useState("");
+  const [currentChar, setCurrentChar] = useState(initialWords.charAt(0));
+  const [incomingChars, setIncomingChars] = useState(initialWords.substring(1));
+  const [startTime, setStartTime] = useState();
+  const [wordCount, setWordCount] = useState(0);
+  const [wpm, setWpm] = useState(0);
+  const [accuracy, setAccuracy] = useState(0);
+  const [typedChars, setTypedChars] = useState("");
+
+  useKeyPress((key) => {
+    if (!startTime) {
+      setStartTime(currentTime());
+    }
+
+    let updatedOutgoingChars = outgoingChars;
+    let updatedIncomingChars = incomingChars;
+
+    if (key === currentChar) {
+      if (incomingChars.charAt(0) === " ") {
+        setWordCount(wordCount + 1);
+        const durationInMinutes = (currentTime() - startTime) / 60000.0;
+        setWpm(((wordCount + 1) / durationInMinutes).toFixed(2));
+      }
+
+      if (leftPadding.length > 0) {
+        setLeftPadding(leftPadding.substring(1));
+      }
+
+      updatedOutgoingChars += currentChar;
+      setOutgoingChars(updatedOutgoingChars);
+
+      setCurrentChar(incomingChars.charAt(0));
+
+      updatedIncomingChars = incomingChars.substring(1);
+
+      if (updatedIncomingChars.split(" ").length < 10) {
+        updatedIncomingChars += " " + generate();
+      }
+      setIncomingChars(updatedIncomingChars);
+    }
+    const updatedTypedChars = typedChars + key;
+    setTypedChars(updatedTypedChars);
+    setAccuracy(
+      ((updatedOutgoingChars.length * 100) / updatedTypedChars.length).toFixed(
+        2
+      )
+    );
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={type} className="App-logo" alt="logo" />
+        <p>Simple typing test to see how fast you can type.</p>
+        <p className="Character">
+          <span className="Character-out">
+            {(leftPadding + outgoingChars).slice(-20)}
+          </span>
+          <span className="Character-current">{currentChar}</span>
+          <span>{incomingChars.substring(0, 20)}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          WPM: {wpm} | Accuracy: {accuracy}%
+        </h3>
       </header>
     </div>
   );
